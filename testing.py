4c99c631309bc2aaa1885d613e18fae6b53ea236
akshay <akshaypol771@gmail.com>
2025-06-20 14:08:54 +0000

    added test

--- testing.py
+++ testing.py
@@ -1,8 +1,14 @@
+"""
+demo.py - Demonstration module
+"""
+
 def hello():
     """
-    this is demo
+    Returns a demo greeting string.
     """
     return "hello dosto"
 
+# Function call should be outside if needed
+if __name__ == "__main__":
+    print(hello())
 
-    hello()
